a2a2f3b9ebf557f7090df16f7511854e3fe5d3d2
Sean Sheng <s3sheng@gmail.com>
2024-01-02 03:24:34 -0800

    chore: make transcribe function sync

--- service.py
+++ service.py
@@ -4,10 +4,8 @@ from pathlib import Path
 
 LANGUAGE_CODE = "en"
 
-whisper_model = bentoml.models.get("whisper-large-v2")
 
-
-@bentoml.service(traffic={"timeout": 1})
+@bentoml.service(traffic={"timeout": 30})
 class BentoWhisperX:    
 
     def __init__(self):
@@ -16,11 +14,11 @@ class BentoWhisperX:
 
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         compute_type = "float16" if torch.cuda.is_available() else "int8"
-        self.model = whisperx.load_model(whisper_model.path, self.device, compute_type=compute_type, language=LANGUAGE_CODE)
+        self.model = whisperx.load_model("large-v2", self.device, compute_type=compute_type, language=LANGUAGE_CODE)
         self.model_a, self.metadata = whisperx.load_align_model(language_code=LANGUAGE_CODE, device=self.device)
 
     @bentoml.api
-    async def transcribe(self, audio_file: Path) -> list:
+    def transcribe(self, audio_file: Path) -> list:
         import whisperx
 
         batch_size = 16 # reduce if low on GPU mem
